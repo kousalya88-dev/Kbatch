fb41d9301a897206d76d663ab1b28fd7f8c2d120
kousalya88-dev <kousi.panneerselvam@gmail.com>
2019-10-18 10:19:58 +0530

    new update

--- src/org/cts/test/ab.java
+++ src/org/cts/test/ab.java
@@ -1,5 +1,13 @@
 package org.cts.test;
 
 public class ab {
+	private void method() {
+		System.out.println("Method");
+
+	}
+	public static void main(String[] args) {
+		ab obj = new ab();
+		obj.method();
+	}
 
 }
